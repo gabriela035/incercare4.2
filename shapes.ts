8ce4e23988cc1453a7bbacbc3964f56a7e97b7d4
gabriela035 <93073180+gabriela035@users.noreply.github.com>
2026-02-06 11:16:35 +0200

    Update shapes.ts

--- shapes.ts
+++ shapes.ts
@@ -1,10 +1,35 @@
-export abstract class Shape {
-    constructor(
-        public x: number, 
-        public y: number, 
-        public color: string
-    ) {}
+import { Shape } from "./Shape.js";
 
-    abstract draw(ctx: CanvasRenderingContext2D): void;
-    abstract isPointInside(px: number, py: number): boolean;
+export class Square extends Shape {
+    size = 70; // Width and Height
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        
+        // Calculate top-left corner so (x,y) remains the center
+        const half = this.size / 2;
+        const topLeftX = this.x - half;
+        const topLeftY = this.y - half;
+
+        ctx.rect(topLeftX, topLeftY, this.size, this.size);
+        
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        
+        // Add a border for better visibility
+        ctx.strokeStyle = "rgba(0,0,0,0.2)";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
+
+    isPointInside(px: number, py: number) {
+        const half = this.size / 2;
+        // Check if mouse is within the bounding box
+        return (
+            px >= this.x - half &&
+            px <= this.x + half &&
+            py >= this.y - half &&
+            py <= this.y + half
+        );
+    }
 }
